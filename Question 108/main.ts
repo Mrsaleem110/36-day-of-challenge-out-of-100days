26f179ca006f09081714d9f630ccd086bf9c0f62
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-05-25 02:05:26 +0500

    adding 36 day of challenge

---
+++ Question 108/main.ts
@@ -0,0 +1,13 @@
+
+//*Question 108:* Compare two strings to check if they are identical, ignoring case sensitivity.
+
+//*Explain & TIP:* To compare strings without considering their case (uppercase/lowercase), you can convert both strings to the same case before comparing.
+
+// This function compares two strings, ignoring case sensitivity
+function areStringsEqualIgnoreCase(str1: string, str2: string): boolean {
+    // Converts both strings to lowercase before comparison
+    return str1.toLowerCase() === str2.toLowerCase();
+  }
+  
+  console.log(areStringsEqualIgnoreCase("hello", "Hello")); 
+  console.log(areStringsEqualIgnoreCase("world", "Word"))
